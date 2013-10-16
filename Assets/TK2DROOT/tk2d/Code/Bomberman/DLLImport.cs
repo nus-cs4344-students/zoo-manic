2579e6e594ab0d0b4346cd0d753ece5d07a2a306
Edwin Goh <edwin_goh88@hotmail.com>
2013-10-16 17:37:11 +0800

    - installed sockio plugin for unity

---
+++ Assets/TK2DROOT/tk2d/Code/Bomberman/DLLImport.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using SocketIOClient;
+using WebSocket4Net;
+using SuperSocket;
+using SimpleJson;
+using System;
+
+public class DLLImport : MonoBehaviour {
+	private Client client;
+
+	// Use this for initialization
+	void Start () {
+		
+		string socketUrl = "http://ec2-54-225-24-113.compute-1.amazonaws.com:5000";
+    	Debug.Log("socket url: " + socketUrl);
+		
+		client = new Client(socketUrl);
+		client.Message += SocketMessage;
+		client.SocketConnectionClosed += SocketConnectionClosed;
+		client.Message +=SocketError;
+		
+		this.client.On("message", (data) => {
+        	Debug.Log("message: " + data);
+    	});
+		
+		client.Connect();
+	}
+	
+	private void SocketOpened(object sender, MessageEventArgs e) {
+    	//invoke when socket opened
+	}
+	
+	private void SocketError(object sender, MessageEventArgs e) {
+    	//invoke when socket encounters error
+	}
+	
+	private void SocketConnectionClosed(object sender, EventArgs e) {
+    	//invoke when socket encounters error
+	}
+	
+	private void SocketMessage (object sender, MessageEventArgs e) {
+    	if ( e!= null && e.Message.Event == "message") {
+       		string msg = e.Message.MessageText;
+       		//process(msg);
+    	}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
